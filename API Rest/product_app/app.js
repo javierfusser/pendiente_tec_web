e22fab788749fe3329a663cfeb607ebec30a3e07
Javier Martínez Rivera <71285956+javierfusser@users.noreply.github.com>
2025-12-01 23:06:01 -0600

    Add files via upload

---
+++ API Rest/product_app/app.js
@@ -0,0 +1,212 @@
+// JSON BASE A MOSTRAR EN FORMULARIO
+var baseJSON = {
+  precio: 0.0,
+  unidades: 1,
+  modelo: "XX-000",
+  marca: "NA",
+  detalles: "NA",
+  imagen: "img/default.png",
+};
+
+$(document).ready(function () {
+  let edit = false;
+
+  let JsonString = JSON.stringify(baseJSON, null, 2);
+  $("#description").val(JsonString);
+  $("#product-result").hide();
+  listarProductos();
+
+  function listarProductos() {
+    $.ajax({
+      url: "./backend/products",
+      type: "GET",
+      success: function (response) {
+        console.log(response);
+        // SE OBTIENE EL OBJETO DE DATOS (ya viene parseado por jQuery)
+        const productos =
+          typeof response === "string" ? JSON.parse(response) : response;
+
+        // SE VERIFICA SI EL OBJETO JSON TIENE DATOS
+        if (Object.keys(productos).length > 0) {
+          // SE CREA UNA PLANTILLA PARA CREAR LAS FILAS A INSERTAR EN EL DOCUMENTO HTML
+          let template = "";
+
+          productos.forEach((producto) => {
+            // SE CREA UNA LISTA HTML CON LA DESCRIPCIÓN DEL PRODUCTO
+            let descripcion = "";
+            descripcion += "<li>precio: " + producto.precio + "</li>";
+            descripcion += "<li>unidades: " + producto.unidades + "</li>";
+            descripcion += "<li>modelo: " + producto.modelo + "</li>";
+            descripcion += "<li>marca: " + producto.marca + "</li>";
+            descripcion += "<li>detalles: " + producto.detalles + "</li>";
+
+            template += `
+                            <tr productId="${producto.id}">
+                                <td>${producto.id}</td>
+                                <td><a href="#" class="product-item">${producto.nombre}</a></td>
+                                <td><ul>${descripcion}</ul></td>
+                                <td>
+                                    <button class="product-delete btn btn-danger">
+                                        Eliminar
+                                    </button>
+                                </td>
+                            </tr>
+                        `;
+          });
+          // SE INSERTA LA PLANTILLA EN EL ELEMENTO CON ID "productos"
+          $("#products").html(template);
+        }
+      },
+    });
+  }
+
+  $("#search").keyup(function () {
+    if ($("#search").val()) {
+      let search = $("#search").val();
+      $.ajax({
+        url: "./backend/products/" + search,
+        type: "GET",
+        success: function (response) {
+          if (!response.error) {
+            // SE OBTIENE EL OBJETO DE DATOS (ya viene parseado por jQuery)
+            const productos =
+              typeof response === "string" ? JSON.parse(response) : response; // SE VERIFICA SI EL OBJETO JSON TIENE DATOS
+            if (Object.keys(productos).length > 0) {
+              // SE CREA UNA PLANTILLA PARA CREAR LAS FILAS A INSERTAR EN EL DOCUMENTO HTML
+              let template = "";
+              let template_bar = "";
+
+              productos.forEach((producto) => {
+                // SE CREA UNA LISTA HTML CON LA DESCRIPCIÓN DEL PRODUCTO
+                let descripcion = "";
+                descripcion += "<li>precio: " + producto.precio + "</li>";
+                descripcion += "<li>unidades: " + producto.unidades + "</li>";
+                descripcion += "<li>modelo: " + producto.modelo + "</li>";
+                descripcion += "<li>marca: " + producto.marca + "</li>";
+                descripcion += "<li>detalles: " + producto.detalles + "</li>";
+
+                template += `
+                                    <tr productId="${producto.id}">
+                                        <td>${producto.id}</td>
+                                        <td><a href="#" class="product-item">${producto.nombre}</a></td>
+                                        <td><ul>${descripcion}</ul></td>
+                                        <td>
+                                            <button class="product-delete btn btn-danger">
+                                                Eliminar
+                                            </button>
+                                        </td>
+                                    </tr>
+                                `;
+
+                template_bar += `
+                                    <li>${producto.nombre}</il>
+                                `;
+              });
+              // SE HACE VISIBLE LA BARRA DE ESTADO
+              $("#product-result").show();
+              // SE INSERTA LA PLANTILLA PARA LA BARRA DE ESTADO
+              $("#container").html(template_bar);
+              // SE INSERTA LA PLANTILLA EN EL ELEMENTO CON ID "productos"
+              $("#products").html(template);
+            }
+          }
+        },
+      });
+    } else {
+      $("#product-result").hide();
+    }
+  });
+
+  $("#product-form").submit((e) => {
+    e.preventDefault();
+
+    // SE CONVIERTE EL JSON DE STRING A OBJETO
+    let postData = JSON.parse($("#description").val());
+    // SE AGREGA AL JSON EL NOMBRE DEL PRODUCTO
+    postData["nombre"] = $("#name").val();
+    postData["id"] = $("#productId").val();
+
+    /**
+     * AQUÍ DEBES AGREGAR LAS VALIDACIONES DE LOS DATOS EN EL JSON
+     * --> EN CASO DE NO HABER ERRORES, SE ENVIAR EL PRODUCTO A AGREGAR
+     **/
+
+    const url = "./backend/product";
+    const method = edit === false ? "POST" : "PUT";
+
+    $.ajax({
+      url: url,
+      type: method,
+      data: postData,
+      success: function (response) {
+        console.log(response);
+        // SE OBTIENE EL OBJETO DE DATOS (ya viene parseado por jQuery)
+        let respuesta =
+          typeof response === "string" ? JSON.parse(response) : response;
+        // SE CREA UNA PLANTILLA PARA CREAR INFORMACIÓN DE LA BARRA DE ESTADO
+        let template_bar = "";
+        template_bar += `
+                        <li style="list-style: none;">status: ${respuesta.status}</li>
+                        <li style="list-style: none;">message: ${respuesta.message}</li>
+                    `;
+        // SE REINICIA EL FORMULARIO
+        $("#name").val("");
+        $("#description").val(JsonString);
+        // SE HACE VISIBLE LA BARRA DE ESTADO
+        $("#product-result").show();
+        // SE INSERTA LA PLANTILLA PARA LA BARRA DE ESTADO
+        $("#container").html(template_bar);
+        // SE LISTAN TODOS LOS PRODUCTOS
+        listarProductos();
+        // SE REGRESA LA BANDERA DE EDICIÓN A false
+        edit = false;
+      },
+    });
+  });
+
+  $(document).on("click", ".product-delete", (e) => {
+    if (confirm("¿Realmente deseas eliminar el producto?")) {
+      const element = $(this)[0].activeElement.parentElement.parentElement;
+      const id = $(element).attr("productId");
+      $.ajax({
+        url: "./backend/product",
+        type: "DELETE",
+        data: { id: id },
+        success: function (response) {
+          $("#product-result").hide();
+          listarProductos();
+        },
+      });
+    }
+  });
+
+  $(document).on("click", ".product-item", (e) => {
+    const element = $(this)[0].activeElement.parentElement.parentElement;
+    const id = $(element).attr("productId");
+    $.ajax({
+      url: "./backend/product/" + id,
+      type: "GET",
+      success: function (response) {
+        // SE CONVIERTE A OBJETO EL JSON OBTENIDO (ya viene parseado por jQuery)
+        let product =
+          typeof response === "string" ? JSON.parse(response) : response;
+        // SE INSERTAN LOS DATOS ESPECIALES EN LOS CAMPOS CORRESPONDIENTES
+        $("#name").val(product.nombre);
+        // EL ID SE INSERTA EN UN CAMPO OCULTO PARA USARLO DESPUÉS PARA LA ACTUALIZACIÓN
+        $("#productId").val(product.id);
+        // SE ELIMINA nombre, eliminado E id PARA PODER MOSTRAR EL JSON EN EL <textarea>
+        delete product.nombre;
+        delete product.eliminado;
+        delete product.id;
+        // SE CONVIERTE EL OBJETO JSON EN STRING
+        let JsonString = JSON.stringify(product, null, 2);
+        // SE MUESTRA STRING EN EL <textarea>
+        $("#description").val(JsonString);
+
+        // SE PONE LA BANDERA DE EDICIÓN EN true
+        edit = true;
+      },
+    });
+    e.preventDefault();
+  });
+});
